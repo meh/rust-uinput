7d3f868cac77df8bafaeaf6d0900a2d95512cbe9
meh <meh@schizofreni.co>
2016-04-25 17:18:07 +0200

    device: remove useless struct

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,14 +1,5 @@
-#[derive(Clone, Copy, PartialEq, Eq, Default, Debug)]
-pub struct Id {
-	pub bus:     u16,
-	pub vendor:  u16,
-	pub product: u16,
-	pub version: u16,
-}
-
 mod builder;
 pub use self::builder::Builder;
 
 mod device;
 pub use self::device::Device;
-
